677a4768e4f3ec56b340a4c4700dd077dc712a4e
mccullen <mccullen@mccullen.xyz>
2024-08-22 15:04:53 -0500

    added lots of stuff

--- models/models.go
+++ models/models.go
@@ -1,19 +1,24 @@
 package models
 
 type RollStats struct {
-	Rolls       []int
-	RollAmounts []int
+	Rolls         []int
+	RollAmounts   []int
+	PreviousInput string
+	CurrentRoll   string
+	Selection     []DiceIdentifier
+}
+
+type DiceIdentifier struct {
+	RollValue   int
+	FromTheLeft int
 }
 
 func NewRollStats() RollStats {
 	return RollStats{
-		Rolls:       []int{},
-		RollAmounts: []int{0, 0, 0, 0, 0, 0},
+		Rolls:         []int{},
+		RollAmounts:   []int{0, 0, 0, 0, 0, 0},
+		PreviousInput: "",
+		CurrentRoll:   "",
+		Selection:     []DiceIdentifier{},
 	}
-
 }
-
-// type PlaceHolderUser struct {
-// 	Email    string `json:"email"`
-// 	Username string `json:"username"`
-// }
